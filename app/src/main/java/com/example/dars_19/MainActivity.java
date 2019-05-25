2c498eb7f1c7b5c2f52bd44536609bd0c0a3f0b6
Akhrorkhoja <akhrorkhoja@mysuperdispatch.com>
2019-05-25 05:04:54 +0500

    Selection with autocomplete textview;

--- app/src/main/java/com/example/dars_19/MainActivity.java
+++ app/src/main/java/com/example/dars_19/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.dars_19;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatAutoCompleteTextView;
 import androidx.appcompat.widget.AppCompatSpinner;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,19 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        spinner = findViewById(R.id.spinner);
-        StateAdapter adapter = new StateAdapter(this, Constants.STATES);
-        spinner.setAdapter(adapter);
-        spinner.setSelected(false);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        AppCompatAutoCompleteTextView autEt = findViewById(R.id.aut_et);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Constants.STATES);
+        autEt.setAdapter(adapter);
+        autEt.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent,
-                                       View view,
-                                       int position,
-                                       long id) {
-                String selectedState = Constants.STATES[position];
-                Toast.makeText(MainActivity.this, "selected:" + selectedState, Toast.LENGTH_SHORT).show();
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
             }
 
             @Override
